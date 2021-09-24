6fe1400c5952566084e3960df3cef252755cf106
Ivan Nesic <fonposta@gmail.com>
2021-09-25 01:16:20 +0200

    add a midi test that does not stop playing for some reason

--- midi_test.py
+++ midi_test.py
@@ -4,7 +4,10 @@ import time
 
 print(mido.get_output_names())
 port = mido.open_output()
-print(port)
-port.send(mido.Message('note_on', note=60))
-time.sleep(5)
-port.send(mido.Message('note_off'))
+# print(port)
+
+
+msg = mido.Message('note_on', note=60, time=5)
+port.send(msg)
+time.sleep(msg.time)
+port.send(mido.Message('note_off', note=60))
