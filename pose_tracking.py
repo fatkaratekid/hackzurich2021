25324d8997c985900e70e4932ba8327beef0e7bb
Ivan Nesic <fonposta@gmail.com>
2021-09-25 16:16:00 +0200

    change unity message format

--- pose_tracking.py
+++ pose_tracking.py
@@ -1,57 +1,19 @@
 import cv2
 import mediapipe as mp
 import socket
+import numpy as np
+
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5065
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
 
-# For static images:
-IMAGE_FILES = []
-BG_COLOR = (192, 192, 192)  # gray
-with mp_pose.Pose(
-        static_image_mode=True,
-        model_complexity=2,
-        enable_segmentation=True,
-        min_detection_confidence=0.5) as pose:
-    for idx, file in enumerate(IMAGE_FILES):
-        image = cv2.imread(file)
-        image_height, image_width, _ = image.shape
-        # Convert the BGR image to RGB before processing.
-        results = pose.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-
-        if not results.pose_landmarks:
-            continue
-        print(
-            f'Nose coordinates: ('
-            f'{results.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].x * image_width}, '
-            f'{results.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].y * image_height})'
-        )
-
-        annotated_image = image.copy()
-        # Draw segmentation on the image.
-        # To improve segmentation around boundaries, consider applying a joint
-        # bilateral filter to "results.segmentation_mask" with "image".
-        condition = np.stack((results.segmentation_mask,) * 3, axis=-1) > 0.1
-        bg_image = np.zeros(image.shape, dtype=np.uint8)
-        bg_image[:] = BG_COLOR
-        annotated_image = np.where(condition, annotated_image, bg_image)
-        # Draw pose landmarks on the image.
-        mp_drawing.draw_landmarks(
-            annotated_image,
-            results.pose_landmarks,
-            mp_pose.POSE_CONNECTIONS,
-            landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
-        cv2.imwrite('/tmp/annotated_image' + str(idx) + '.png', annotated_image)
-        # Plot pose world landmarks.
-        mp_drawing.plot_landmarks(
-            results.pose_world_landmarks, mp_pose.POSE_CONNECTIONS)
+# hand_down_overlay = cv2.imread('hand_down.png', cv2.IMREAD_UNCHANGED)
 
 # For webcam input:
 cap = cv2.VideoCapture(0)
@@ -73,25 +35,25 @@ with mp_pose.Pose(
         image.flags.writeable = False
         results = pose.process(image)
 
-
         if results.pose_landmarks:
             pose_landmarks = results.pose_landmarks.ListFields()[0][1]
             left_hand = pose_landmarks[20]
             right_hand = pose_landmarks[19]
 
-            unity_json = str({
-                'left_hand_x': left_hand.x,
-                'left_hand_y': left_hand.y,
-                'right_hand_x': right_hand.x,
-                'right_hand_y': right_hand.y,
-            })
+            unity_data_string = f'left_hand {left_hand.x} left_hand_y {left_hand.y} right_hand_x {right_hand.x} right_hand_y {right_hand.y}'
+            # unity_json = str({
+            #     'left_hand_x': left_hand.x,
+            #     'left_hand_y': left_hand.y,
+            #     'right_hand_x': right_hand.x,
+            #     'right_hand_y': right_hand.y,
+            # })
 
             sock.sendto(
-                (unity_json).encode(),
+                (unity_data_string).encode(),
                 (UDP_IP, UDP_PORT)
             )
 
-            print(unity_json)
+            print(unity_data_string)
             # print("_" * 10, "Jump Action Triggered!", "_" * 10)
 
         # Draw the pose annotation on the image.
@@ -102,6 +64,11 @@ with mp_pose.Pose(
             results.pose_landmarks,
             mp_pose.POSE_CONNECTIONS,
             landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+
+        # print(hand_down_overlay.shape)
+        # added_image = logo_overlay(image, hand_down_overlay, scale=0.5, y=-100, x=-100)
+        # added_image = image * (1 - hand_down_overlay[:, :, 3:]) + hand_down_overlay[:, :, 3:]
+        # added_image = cv2.addWeighted(image, 1., hand_down_overlay, 1., 0)
         cv2.imshow('MediaPipe Pose', image)
         if cv2.waitKey(5) & 0xFF == 27:
             break
